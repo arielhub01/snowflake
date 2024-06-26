58c09644bb67aa123b70e50d5ed90954347b90b7
Ariel Yumul <ayumul@eqc.govt.nz>
2024-06-26 13:58:26 +1200

    testing now

--- providers.tf
+++ providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_providers {
-    snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.63"
-    }
-  }
+#   required_providers {
+#     snowflake = {
+#       source  = "Snowflake-Labs/snowflake"
+#       version = "~> 0.63"
+#     }
+#   }
   backend "remote" {
     organization = "snow-tfc-github-org"
 
@@ -19,5 +19,4 @@ provider "snowflake" {
   user     = var.snowflake_username
   password = var.snowflake_password
   role     = "ACCOUNTADMIN"
-  region = "australiaeast.azure"
 }
