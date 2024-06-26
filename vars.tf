d10ea9f10c545b541eddee8a115147f01fceed36
Ariel Yumul <ayumul@eqc.govt.nz>
2024-06-26 12:03:52 +1200

    rem vars

--- vars.tf
+++ vars.tf
@@ -1,10 +1,10 @@
-variable "snowflake_username" {
-  description = "User"
-  type        = string
-  sensitive   = true
-}
-variable "snowflake_password" {
-  description = "Password"
-  type        = string
-  sensitive   = true
-}
+# variable "snowflake_username" {
+#   description = "User"
+#   type        = string
+#   sensitive   = true
+# }
+# variable "snowflake_password" {
+#   description = "Password"
+#   type        = string
+#   sensitive   = true
+# }
