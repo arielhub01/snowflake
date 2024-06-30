3896f83290e7b5a12ed75763627df3d3eb266711
Ariel Yumul <ayumul@eqc.govt.nz>
2024-07-01 10:29:04 +1200

    testing more

--- vars.tf
+++ vars.tf
@@ -2,17 +2,16 @@ variable "snowflake_username" {
   description = "User"
   type        = string
   sensitive   = true
-  default     = ""
 }
 variable "snowflake_password" {
   description = "Password"
   type        = string
   sensitive   = true
-  default     = ""
+
 }
 variable "snowflake_acct" {
   description = "Account"
   type        = string
   sensitive   = true
-  default     = ""
+
 }
