e86d9e45be59a4b4900cab852054f65057085246
Ariel Yumul <ayumul@eqc.govt.nz>
2024-07-02 14:28:28 +1200

    testing now

--- modules/snowdb/snowdb.tf
+++ modules/snowdb/snowdb.tf
@@ -2,3 +2,12 @@ resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
+
+terraform {
+   required_providers {
+     snowflake = {
+       source  = "Snowflake-Labs/snowflake"
+       version = "~> 0.87"
+     }
+   }
+}
