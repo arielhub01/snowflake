9aecd8c29def2b4aa59ee91cce353340e0b878ca
Ariel Yumul <ayumul@eqc.govt.nz>
2024-07-03 10:16:58 +1200

    update on vmod

--- modules/snowdb/snowdb.tf
+++ modules/snowdb/snowdb.tf
@@ -6,12 +6,3 @@ resource "snowflake_database" "new_db" {
   name    = "NEW_DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
-
-terraform {
-   required_providers {
-     snowflake = {
-       source  = "Snowflake-Labs/snowflake"
-       version = "~> 0.87"
-     }
-   }
-}
