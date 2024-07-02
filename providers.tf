7943784540dbdd22983ad83bc411ae1c4aea631d
Ariel Yumul <ayumul@eqc.govt.nz>
2024-07-03 09:40:31 +1200

    update provider

--- providers.tf
+++ providers.tf
@@ -1,7 +1,9 @@
 provider "snowflake" {
-  account  = var.snowflake_acct
+  account  = "XU93738.australia-east.azure"
   user     = var.snowflake_username
   password = var.snowflake_password
   role     = "ACCOUNTADMIN"
+  region = "australia-east.azure"
+  request_timeout = "10m"
     
 }
