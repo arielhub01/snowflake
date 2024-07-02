5081a3dc4dd95bbd96fe2a6ae3cb8f8d50ecedee
Ariel Yumul <ayumul@eqc.govt.nz>
2024-07-02 14:01:15 +1200

    test sorce provider

--- providers.tf
+++ providers.tf
@@ -1,4 +1,6 @@
 provider "snowflake" {
+  source      = "Snowflake-Labs/snowflake"
+  version     = "0.92.0"
   account  = var.snowflake_acct
   user     = var.snowflake_username
   password = var.snowflake_password
