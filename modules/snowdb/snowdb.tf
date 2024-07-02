0d327fccc0e4fbc1fef6fcbeefa1895716afbf33
Ariel Yumul <ayumul@eqc.govt.nz>
2024-07-03 09:30:05 +1200

    added db

--- modules/snowdb/snowdb.tf
+++ modules/snowdb/snowdb.tf
@@ -2,6 +2,10 @@ resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
+resource "snowflake_database" "new_db" {
+  name    = "NEW_DEMO_DB"
+  comment = "Database for Snowflake Terraform demo"
+}
 
 terraform {
    required_providers {
