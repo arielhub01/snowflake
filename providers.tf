d580554b117d25cbff8c1db1d656680a21b64458
Ariel Yumul <ayumul@eqc.govt.nz>
2024-07-02 13:50:51 +1200

    move version

--- providers.tf
+++ providers.tf
@@ -1,12 +1,3 @@
-terraform {
-   required_providers {
-     snowflake = {
-       source  = "Snowflake-Labs/snowflake"
-       version = "0.92.0"
-     }
-   }
-}
-
 provider "snowflake" {
   account  = var.snowflake_acct
   user     = var.snowflake_username
