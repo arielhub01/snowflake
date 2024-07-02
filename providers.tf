efb21654d3d745a697a33ba8461076c344cbf040
Ariel Yumul <ayumul@eqc.govt.nz>
2024-07-02 14:04:54 +1200

    cleanup prvder

--- providers.tf
+++ providers.tf
@@ -1,6 +1,4 @@
 provider "snowflake" {
-  source      = "Snowflake-Labs/snowflake"
-  version     = "0.92.0"
   account  = var.snowflake_acct
   user     = var.snowflake_username
   password = var.snowflake_password
